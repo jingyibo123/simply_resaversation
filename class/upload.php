2f0e8cd3e2a4d10754da7a3af4aef3789e59c83a
laelia44 <laelia.lecharpentier@free.fr>
2015-05-08 16:01:24 +0200

    redimensionnement image pas bon

--- class/upload.php
+++ class/upload.php
@@ -2,10 +2,12 @@
 include 'include/header.inc.php';
 
 require_once 'class/bdd.class.php';
+require_once 'class/restaurant.class.php';
 
 $iId = $_GET['id'];
 
 $oBdd = new Bdd();
+$oImage = new Restaurant();
 
 
 // Tests fichier bien envoyé et pas d'erreur
@@ -14,11 +16,38 @@ if (isset($_FILES['image']) AND $_FILES['image']['error'] == 0) {
         if ($_FILES['image']['size'] <= 500000) {
                 $infosImage = pathinfo($_FILES['image']['name']);
                 $extension_upload = $infosImage['extension'];
-                $extensions_autorisees = array('jpg', 'jpeg', 'gif', 'png');
+                $extensions_autorisees = array('gif', 'jpg', 'png');
                 if (in_array($extension_upload, $extensions_autorisees)) {
 					move_uploaded_file($_FILES['image']['tmp_name'], 'images/' . basename($_FILES['image']['name']));
+//					$image2 = $oImage->redimensionner(400, 400, '', '', 'images', basename($_FILES['image']['name']))
 					$sModifImage = $oBdd->updateImage($iId, $_FILES['image']['name']);
-                    echo "Image modifiée !";?>
+					echo "Image modifiée !";
+					
+/*					if ($extension_upload == $extensions_autorisees[0]){
+						// Image GIF
+						$image1 = imagecreatefromgif($_FILES['image']['name']);
+						$image2 = imagecreate(60,60);
+						move_uploaded_file($image2, 'images/' . basename($_FILES['image']['name']));
+						$sModifImage = $oBdd->updateImage($iId, $_FILES['image']['name']);
+						echo "Image modifiée !";
+						}elseif ($extension_upload == $extensions_autorisees[1]){
+							// Image JPG
+							$image1 = imagecreatefromjpeg($_FILES['image']['name']);
+							$image2 = imagecreate(60,60);
+							move_uploaded_file($image2, 'images/' . basename($_FILES['image']['name']));
+							$sModifImage = $oBdd->updateImage($iId, $_FILES['image']['name']);
+							echo "Image modifiée !";
+						}elseif ($extension_upload == $extensions_autorisees[2]){
+							// Image png
+							$image1 = imagecreatefrompng($_FILES['image']['name']);
+							$image2 = imagecreate(60,60);
+							move_uploaded_file($image2, 'images/' . basename($_FILES['image']['name']));
+							$sModifImage = $oBdd->updateImage($iId, $_FILES['image']['name']);
+							echo "Image modifiée !";
+						}else{
+							echo'Format non accepté';
+						}**/ ?>
+						
 					<html>
 					<body>
 						<p><a href="index.php?category=4">Retour au menu</a></p>
--- view/modificationImage.view.php
+++ view/modificationImage.view.php
@@ -9,7 +9,7 @@ $iId = $_GET['id'];
 ?>
 
 <form method="post" action="<?php echo $_SERVER['PHP_SELF'].'?category=16&&id='.$iId; ?>" enctype="multipart/form-data">
-     <label for="image">Modification de l'image (Formats autorisés : jpeg / Taille maximale : 10Mo) :</label><br />
+     <label for="image">Modification de l'image (Formats autorisés : jpg, png, gif / Taille maximale : 10Mo) :</label><br />
 	 <input type="hidden" name=\"max_file_size" value="500000">
      <input type="file" name="image" /><br />
      <input type="submit" name="submit" value="Modifier" />
