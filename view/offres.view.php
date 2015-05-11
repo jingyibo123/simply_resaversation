fa3b6bb4f7db87218e0b2e93e1f6937d097edb46
mpoiri13 <marin.pc@live.fr>
2015-05-11 14:33:02 +0200

    changement offres.view

--- view/offres.view.php
+++ view/offres.view.php
@@ -11,7 +11,7 @@ include 'include/header.inc.php';
 	</head> 
 	<body>
 		<?php if($_SESSION['droit']==1){
-			?><p><a href="index.php?category=7"> Ajouter une offre</a></p><?php
+			?><p><a href="index.php?category=18"> Ajouter une offre</a></p><?php
 		}
 		?>
 		<p><a href="index.php?category=4"> Retour au menu</a></p>
