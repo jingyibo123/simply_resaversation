31a4bb7d0c2d6f236363a411c88c4db452f34cc7
mpoiri13 <marin.pc@live.fr>
2015-05-11 13:58:08 +0200

    Create messageAjoutOffre.view.php

---
+++ view/messageAjoutOffre.view.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html> 
+
+<?php
+include 'include/header.inc.php';
+?>
+
+<html>
+    <head>
+		<meta charset="utf-8" /> 
+		<title>Ajout Offre</title> 
+	</head> 
+	<body>
+		<h3>L'offre a bien été ajoutée</h3>
+		<p><a href="index.php?category=4">Retour au menu</a></p>
+	</body>
+</html>
+
+<?php
+include 'include/footer.inc.php';
+?>
