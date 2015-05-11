45f263b52fad13f4a809af4b1c0e6469a1fec042
mpoiri13 <marin.pc@live.fr>
2015-05-11 14:38:47 +0200

    ajout bouton retour menu

--- view/ajoutOffre.view.php
+++ view/ajoutOffre.view.php
@@ -16,13 +16,16 @@ $iId = $_GET['id'];
 
     
     <div>
-        <p>Descriptif : </p><textarea name="modification[descriptif]" rows="7" cols="100"><?php echo ''.$aRestaurant['DESCRIPTIF']; ?></textarea> <?php if(isset($oOffre->aError['descriptif'])){echo $oOffre->aError['descriptif'];} ?>
+        <p>Descriptif : </p><textarea name="ajout[descriptif]" rows="7" cols="100"></textarea> <?php if(isset($oOffre->aError['descriptif'])){echo $oOffre->aError['descriptif'];} ?>
     </div>
 
     <p></br><input name="Ajouter" value="Ajouter" type="submit" /></p>
+	
+	
 </form>
-
+<p><a href="index.php?category=4"> Retour au menu</a></p>
 
 <?php
 include 'include/footer.inc.php';
 ?>
+
