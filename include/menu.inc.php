e925b051b30e66a2af68b62f147650a83b3df747
mpoiri13 <marin.pc@live.fr>
2015-05-19 18:14:40 +0200

    changement

--- include/menu.inc.php
+++ include/menu.inc.php
@@ -28,7 +28,7 @@ $sPrenom = $aMembre['PRENOM'];
 					case "2": // Restaurateur : A créer
 					?>
 						<h2>Compte restaurateur : Bienvenue <?php echo $sPrenom.' '.$sNom;?> ! </h2>
-						<li><a href="index.php?category=28&&id=<?php echo $_SESSION['id_user']; ?>"> Mon Compte </a></li>
+						<li><a href="index.php?category=28&&id=<?php echo $_SESSION['id_user']; ?>"> Modifier le mot de passe </a></li>
 						<li><a href="index.php?category=10"> Mes Restaurants </a></li>
 						<li><a href="index.php?category=12"> Mes Offres </a></li>
 						<li><a href="index.php?category=11"> Mes Reservations </a></li>
@@ -38,11 +38,11 @@ $sPrenom = $aMembre['PRENOM'];
 					case "1": // Administrateur : A créer
 					?>
 						<h2>Compte administrateur : Bienvenue <?php echo $sPrenom.' '.$sNom;?> ! </h2>
-						<li><a href="index.php?category=28&&id=<?php echo $_SESSION['id_user']; ?>"> Mon Compte </a></li>
+						<li><a href="index.php?category=28&&id=<?php echo $_SESSION['id_user']; ?>"> Modifier le mot de passe </a></li>
 						<li><a href="index.php?category=7"> Restaurateurs </a></li>
 						<li><a href="index.php?category=5"> Restaurants </a></li>
 						<li><a href="index.php?category=30"> Notifications</a></li>
-						<li><a href="index.php?category=41"> Inscrire un membre</a></li>
+						<li><a href="index.php?category=41"> Inscrire un restaurateur</a></li>
 						<li><a href="index.php?category=100" onclick="return confirm('Voulez-vous vraiment vous deconnecter ?');">Deconnexion</a></li>
 					<?php break;
 					
