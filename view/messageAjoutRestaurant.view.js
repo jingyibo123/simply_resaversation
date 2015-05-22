ef4e288f70bf633fc2a8110006b4d814851205c1
jingyibo123 <jingyibo123@hotmail.com>
2015-05-22 08:45:00 +0200

    enlever merde

--- view/messageAjoutRestaurant.view.js
+++ view/messageAjoutRestaurant.view.js
@@ -11,7 +11,6 @@ $(document).ready(function() {
 	$ ("input#nbtablesrange").change (function (){
 		$ ("input#nbtables").val($ ("input#nbtablesrange").val());
 	});
-	alert("merde");
 
 	$("button#btnsavecalendar").click(function(){
 		var jours = new Array();
